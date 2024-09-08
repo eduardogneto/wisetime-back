725cf8336ce19c688eef61eb007d4d8e875bee6f
eduardo.neto <edu.g.neto06@gmail.com>
2024-09-07 21:27:27 -0300

    inicio do ambiente

---
+++ src/main/java/com/wisetime/wisetime/WisetimeApplication.java
@@ -0,0 +1,13 @@
+package com.wisetime.wisetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisetimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisetimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wisetime/wisetime/WisetimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisetime.wisetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisetimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
