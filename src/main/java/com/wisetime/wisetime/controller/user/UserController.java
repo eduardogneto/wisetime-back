b747c81639424e45dac5164d0ffd2b1ee7fa324f
eduardo.neto <edu.g.neto06@gmail.com>
2024-10-05 20:44:33 -0300

    retirando comentarios de metodos antigos

--- src/main/java/com/wisetime/wisetime/controller/user/UserController.java
+++ src/main/java/com/wisetime/wisetime/controller/user/UserController.java
@@ -1,21 +1,15 @@
 package com.wisetime.wisetime.controller.user;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wisetime.wisetime.DTO.team.TeamDTO;
-import com.wisetime.wisetime.DTO.user.UserLoginDTO;
-import com.wisetime.wisetime.DTO.user.UserRegisterDTO;
 import com.wisetime.wisetime.DTO.user.UserResponseDTO;
 import com.wisetime.wisetime.service.team.TeamService;
 import com.wisetime.wisetime.service.user.UserService;
@@ -29,27 +23,6 @@ public class UserController {
 
     @Autowired
     private TeamService teamService;
-
-//    @PostMapping("/register")
-//    public ResponseEntity<?> registerOrUpdate(@RequestBody UserRegisterDTO registerDTO) {
-//        try {
-//            UserResponseDTO savedUserDTO = userService.registerOrUpdate(registerDTO);
-//            return ResponseEntity.ok(savedUserDTO);
-//        } catch (RuntimeException e) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-//        }
-//    }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<?> login(@RequestBody UserLoginDTO loginDTO) {
-//        Optional<UserResponseDTO> userDTOOpt = userService.login(loginDTO.getEmail(), loginDTO.getPassword());
-//
-//        if (userDTOOpt.isPresent()) {
-//            return ResponseEntity.ok(userDTOOpt.get());
-//        } else {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email ou senha inválidos");
-//        }
-//    }
 
     @GetMapping("/organization")
     public ResponseEntity<List<UserResponseDTO>> getUsersByOrganization(@RequestParam Long organizationId) {
--- src/main/java/com/wisetime/wisetime/service/user/UserService.java
+++ src/main/java/com/wisetime/wisetime/service/user/UserService.java
@@ -8,12 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wisetime.wisetime.DTO.team.TeamDTO;
-import com.wisetime.wisetime.DTO.user.UserRegisterDTO;
 import com.wisetime.wisetime.DTO.user.UserResponseDTO;
 import com.wisetime.wisetime.models.organization.Organization;
 import com.wisetime.wisetime.models.team.Team;
 import com.wisetime.wisetime.models.user.User;
-import com.wisetime.wisetime.repository.team.TeamRepository;
 import com.wisetime.wisetime.repository.user.UserRepository;
 
 @Service
@@ -21,65 +19,16 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private TeamRepository teamRepository;
     
     public Optional<User> findEntityById(Long userId) {
         return userRepository.findById(userId); 
     }
 
-//    public User saveOrUpdateUser(User user) {
-//        Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
-//
-//        if (existingUser.isPresent() && user.getId() == null) {
-//            throw new RuntimeException("Usuário já existe.");
-//        }
-//
-//        if (!existingUser.isPresent() && user.getId() != null) {
-//            throw new RuntimeException("Usuário não existe.");
-//        }
-//
-//        if (user.getTag() == null) {
-//            throw new RuntimeException("Necessário inserir uma tag.");
-//        }
-//
-//        return userRepository.save(user);
-//    }
-//
-//    public Optional<UserResponseDTO> login(String email, String password) {
-//        Optional<User> userOpt = userRepository.findByEmail(email);
-//        if (userOpt.isPresent()) {
-//            User user = userOpt.get();
-//            if (user.getPassword().equals(password)) {
-//                return Optional.of(mapToDTO(user)); 
-//            }
-//        }
-//        return Optional.empty();
-//    }
 
     public List<UserResponseDTO> getUsersByOrganization(Long organizationId) {
         List<User> users = userRepository.findByTeam_Organization_Id(organizationId);
         return users.stream().map(this::mapToDTO).collect(Collectors.toList()); 
     }
-
-//    public UserResponseDTO registerOrUpdate(UserRegisterDTO registerDTO) {
-//        User user = new User();
-//        user.setId(registerDTO.getId());
-//        user.setName(registerDTO.getName());
-//        user.setEmail(registerDTO.getEmail());
-//        user.setPassword(registerDTO.getPassword());
-//        user.setTag(registerDTO.getTag());
-//
-//        Team team = teamRepository.findById(registerDTO.getTeamId())
-//                .orElseThrow(() -> new RuntimeException("Nenhum cargo encontrado para o id: " + registerDTO.getTeamId()));
-//
-//        user.setTeam(team);
-//
-//        User savedUser = saveOrUpdateUser(user);
-//
-//        return mapToDTO(savedUser); 
-//    }
 
     
     
