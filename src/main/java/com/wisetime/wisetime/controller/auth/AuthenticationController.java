697740516a9f0545b53a7f8018d45f23c67804fb
eduardo.neto <edu.g.neto06@gmail.com>
2024-10-26 23:01:53 -0300

    adicionando editar usuario

--- src/main/java/com/wisetime/wisetime/controller/auth/AuthenticationController.java
+++ src/main/java/com/wisetime/wisetime/controller/auth/AuthenticationController.java
@@ -7,7 +7,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.wisetime.wisetime.DTO.auth.AuthenticationDTO;
 import com.wisetime.wisetime.DTO.auth.LoginResponseDTO;
 import com.wisetime.wisetime.DTO.auth.RegisterDTO;
+import com.wisetime.wisetime.DTO.user.EditUserDTO;
 import com.wisetime.wisetime.DTO.user.UserResponseDTO;
 import com.wisetime.wisetime.infra.security.TokenService;
 import com.wisetime.wisetime.models.team.Team;
 import com.wisetime.wisetime.models.user.User;
-import com.wisetime.wisetime.repository.user.UserRepository;
 import com.wisetime.wisetime.repository.team.TeamRepository;
+import com.wisetime.wisetime.repository.user.UserRepository;
 import com.wisetime.wisetime.service.user.UserService;
 
 import jakarta.validation.Valid;
@@ -78,6 +81,42 @@ public class AuthenticationController {
     	this.userRepository.save(newUser);
     	
     	return ResponseEntity.ok().build();	
+    }
+    
+    @PutMapping("/users/{id}")
+    public ResponseEntity<?> editUser(@PathVariable Long id, @RequestBody @Valid EditUserDTO data) {
+        try {
+            User existingUserWithEmail = (User) userRepository.findByEmail(data.getEmail());
+            if (existingUserWithEmail != null && !existingUserWithEmail.getId().equals(id)) {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Email já está em uso.");
+            }
+
+            User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado para o ID: " + id));
+
+            user.setName(data.getName());
+            user.setEmail(data.getEmail());
+            user.setTag(data.getTag());
+
+            Team team = teamRepository.findById(data.getTeamId())
+                .orElseThrow(() -> new RuntimeException("Time não encontrado para o ID: " + data.getTeamId()));
+            user.setTeam(team);
+
+            if(data.getPassword() != null && !data.getPassword().isEmpty()) {
+                String encryptedPassword = new BCryptPasswordEncoder().encode(data.getPassword());
+                user.setPassword(encryptedPassword);
+            }
+
+            userRepository.save(user);
+
+            UserResponseDTO userResponseDTO = userService.mapToDTO(user);
+
+            return ResponseEntity.ok(userResponseDTO);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao editar usuário.");
+        }
     }
     
     @GetMapping("/validate-token")
